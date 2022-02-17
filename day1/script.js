0800c9f91e2f5b76ec7af46739d8900037f380fb
Masturah Ghali <ronkeghali@gmail.com>
2022-02-17 10:56:01 +0100

    Added the day one completed html, css and Javascript project

---
+++ day1/script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll('.panel')
+
+for (let i = 0; i < panels.length; i++){
+    panels[i].addEventListener('click', () => {
+        removeActiveClasses();
+        panels[i].classList.add('active');
+    })
+}
+removeActiveClasses = () => {
+for (let i = 0; i < panels.length; i++){
+    panels[i].classList.remove('active')
+}
+}
